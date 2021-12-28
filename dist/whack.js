155f695136f8cd2865e5395f5f8af48dcc8c88f8
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2021-12-28 11:25:01 -0600

    favicon change, timer update and JS, timer does not stop

--- dist/whack.js
+++ dist/whack.js
@@ -1,25 +1,76 @@
 let hits = 0;
 let score = 0;
-let timeLeft = 60;
+let timer = 59;
+let start = document.getElementById('start');
+let timeLeft = document.getElementById('timeLeft');
+let topScore = 1;
+let secondScore = 0;
+let thirdScore = 0;
+let gameBoard = document.querySelectorAll(".whackBox");
+let timer = 29
+let start = document.getElementById("start");
+let moleHoles = document.querySelectorAll(".hole")
+let moles = document.querySelectorAll(".mole");
 
 
 
-function startButton() {
-    let sixtySeconds = 60,
-        display = document.querySelector('#time');
-    startTimer(sixtySeconds, display);
-};
+start.addEventListener("click", () => {
+    start.classList.add('removeStart')
+    let timeLeft = window.setInterval(()=> {
+        timeLeft.innerText = timer;
+        timer--;
+    }, 1000);
+    let whereMole = window.setInterval(()=> {
+        moleLocation();
+        moles = document.querySelectorAll('.mole');
+        return moles;
+    }, 1000);
+    window.setTimeout(()=> {
+        window.clearInterval(whereMole);
+        window.clearInterval(timeLeft);
+        start.classList.remove('removeStart');
+        score =1
+        timeLeft.innerHTML = 60;
+        timer = 59
+    }, 31005);
+});
 
-document.getElementById("start").addEventListener("click", startTimer);
+const moleLocation = () => {
+    moleHoles[Math.floor(Math.random() * moleHoles.length)].classList.add('mole');
+}
+
+moleHoles.forEach((val)=>{
+    val.addEventListener('click', (e) => {
+        document.getElementById('score').innerText = score;
+        if (e.target.classList.contains("mole")) {
+            e.target.classList.replace("mole", "splat")
+            score++;
+        if (score >= topScore) {
+            topScore = score - 1;
+            document.getElementById("score1").innerText = topScore;
+            console.log(topScore);
+        } else if (score >= secondScore) {
+            secondScore = score - 1;
+            document.getElementById("score2").innerText = score;
+        } else if (score >= thirdScore) {
+            thirdScore = score - 1;
+            document.getElementById("score3").innerText = score;
+        }
+    }
+})
+});
+let resetHoles = window.setInterval(() => {
+    let splats = document.querySelectorAll(".splat");
+    splats.forEach((val) => {
+        val.classList.replace("splat", "hole");
+    })
+}, 2000)
 
 
 
 function startTimer(duration, display) {
-document.getElementById("start").removeEventListener("click", startTimer);  
- 
     var timer = duration,
         seconds;
-
     setInterval(function() {
 
         seconds = parseInt(timer % 60, 10);
@@ -31,17 +82,19 @@ document.getElementById("start").removeEventListener("click", startTimer);
         if (--timer < 0) {
             timer = duration;
         }
-        
     }, 1000);
 }
 
+window.onload = function() {
+    let sixtySeconds = 60,
+        display = document.querySelector('#time');
+    startTimer(sixtySeconds, display);
+};
 
 
 
 
 
-
-document.getElementById("button").addEventListener("click", moleAppear1, true);
 
 const holes = document.querySelectorAll(".div");
 
@@ -55,6 +108,10 @@ function moleAppear() {
         }, 1000);
 
     }
+
+
+
+
 
 window.onload = function() {
     let sixtySeconds = 60,
