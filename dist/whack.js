ac3bb11f7d2eda81067e0241d8c81a8db113ec36
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2021-12-28 19:01:51 -0600

    rename pics to mole, timer stops,

--- dist/whack.js
+++ dist/whack.js
@@ -2,48 +2,43 @@ let hits = 0;
 let score = 0;
 let timer = 59;
 let start = document.getElementById('start');
-let timeLeft = document.getElementById('timeLeft');
-let topScore = 1;
-let secondScore = 0;
-let thirdScore = 0;
-let gameBoard = document.querySelectorAll(".whackBox");
-let timer = 29
-let start = document.getElementById("start");
-let moleHoles = document.querySelectorAll(".hole")
+let time = document.getElementById('time');
+let wBox2 = document.querySelectorAll(".wBox2")
+let holes = document.querySelectorAll(".hole")
 let moles = document.querySelectorAll(".mole");
 
-
-
 start.addEventListener("click", () => {
-    start.classList.add('removeStart')
-    let timeLeft = window.setInterval(()=> {
-        timeLeft.innerText = timer;
+
+    document.getElementById('start').style.visibility = "hidden";
+    document.getElementsByClassName('wBox2')[0].style.visibility = "visible";
+    let time = window.setInterval(()=> {
+        document.getElementById("time").innerText = ':' +  timer;
         timer--;
     }, 1000);
     let whereMole = window.setInterval(()=> {
-        moleLocation();
-        moles = document.querySelectorAll('.mole');
+        // moleLocation();
+        moles = document.querySelectorAll('.sunmole');
         return moles;
     }, 1000);
     window.setTimeout(()=> {
         window.clearInterval(whereMole);
-        window.clearInterval(timeLeft);
-        start.classList.remove('removeStart');
+        window.clearInterval(time);
+       // document.getElementById('start').style.visibility = "visible";
         score =1
-        timeLeft.innerHTML = 60;
+        time.innerHTML = 60;
         timer = 59
-    }, 31005);
+    }, 60900);
 });
 
 const moleLocation = () => {
-    moleHoles[Math.floor(Math.random() * moleHoles.length)].classList.add('mole');
+    holes[Math.floor(Math.random() * holes.length)].classList.add('sunmole');
 }
 
-moleHoles.forEach((val)=>{
+holes.forEach((val)=>{
     val.addEventListener('click', (e) => {
         document.getElementById('score').innerText = score;
-        if (e.target.classList.contains("mole")) {
-            e.target.classList.replace("mole", "splat")
+        if (e.target.classList.contains("sunmole")) {
+            e.target.classList.replace("sunmole", "splat")
             score++;
         if (score >= topScore) {
             topScore = score - 1;
@@ -66,215 +61,12 @@ let resetHoles = window.setInterval(() => {
     })
 }, 2000)
 
-
-
-function startTimer(duration, display) {
-    var timer = duration,
-        seconds;
-    setInterval(function() {
-
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-window.onload = function() {
-    let sixtySeconds = 60,
-        display = document.querySelector('#time');
-    startTimer(sixtySeconds, display);
-};
-
-
-
-
-
-
-const holes = document.querySelectorAll(".div");
-
-function moleAppear() {
-    let active = holes[Math.floor(Math.random() * holes.length)];
-    active.classList.add("theMole");
-
-     setTimeout(() => {
-          active.classList.remove("theMole" );
-        setTimeout(moleAppear1, 100);
-        }, 1000);
-
-    }
-
-
-
-
-
-window.onload = function() {
-    let sixtySeconds = 60,
-        display = document.querySelector("#time");
-    startTimer(sixtySeconds, display);
-};
-
-let counter = window.setInterval(() => {
-    document.getElementById("timer").innerText = timeLeft;
-    timeLeft--;
-    if (timeLeft < 0) {
-        window.clearInterval(counter);
-        window.clearInterval(whereMole);
-    }
-}, 1000);
-
-function countDown() {
-    let x = setTimeout(countdown, 1000);
-
-    let time = document.getElementById("timer");
-    let score = document.getElementById(scored);
-
-    time.innerHTML = "TIME LEFT:    " + timeLeft + " seconds";
-
-    if (timeLeft < 1) {
-        clearTimeout(x);
-        window.clearInterval(whereMole);
-        window.clearInterval(timeLeft);
-        time.innerHTML = "TIME'S UP";
-        score.innerHTML = hits;
-    }
-    timeLeft--;
-}
-
-const moleClass = document.querySelectorAll(".moleHole");
-let active = moleClass[Math.floor(Math.random() * moleClass.length)];
-const moleLocation = () => {
-    moleClass[Math.floor(Math.random() * moleClass.length)].classList.replace("moleHole", "theMole");
-};
-let whereMole = window.setInterval(() => {
-    moleLocation();
-    let moles = document.querySelectorAll(".theMole");
-    moles.forEach((val) => {
-        val.addEventListener("click", (e) => {
-            e.target.classList.replace("theMole", "theSplat");
-
-            resetHoles = window.setTimeout(() => {
-                let splats = document.querySelectorAll(".theSplat");
-                splats.forEach((val) => {
-                    val.classList.replace("theSplat", "moleHole");
-                });
-            }, 2000);
-        });
-    });
-}, 1000);
-
-// active.addEventListener("click", score, false);
-
-// let score = 0;
-
-// function score(){
-//     let result;
-//     if (class is active){let score = score + 1;
-// }else{
-//     if ()
-
-// }
-
-// function moleAppear1() {
-//     active = moleClass[Math.floor(Math.random() * moleClass.length)];
-
-//     active.classList.add("theMole");
-
-//     setTimeout(() => {
-//         active.classList.remove("theMole");
-//         setTimeout(moleAppear1, 100);
-//     }, 1000);
-// }
-// moleAppear1();
-
-// function moleAppear2() {
-
-//     let active = moleClass[Math.floor(Math.random() * moleClass.length)];
-
-//     active.classList.add("theMole");
-
-//     setTimeout(() => {
-//         active.classList.remove("theMole");
-//         setTimeout(moleAppear2, 200);
-//     }, 1000);
-// }
-// moleAppear2();
-
-// function moleAppear3() {
-//     let active = moleClass[Math.floor(Math.random() * moleClass.length)];
-
-//     active.classList.add("theMole");
-
-//     setTimeout(() => {
-//         active.classList.remove("theMole");
-//         setTimeout(moleAppear3, 300);
-//     }, 1000);
-// }
-// moleAppear3();
-
-// function moleAppear4() {
-//     let active = moleClass[Math.floor(Math.random() * moleClass.length)];
-
-//     active.classList.add("theMole");
-
-//     setTimeout(() => {
-//         active.classList.remove("theMole");
-//         setTimeout(moleAppear4, 400);
-//     }, 1000);
-// }
-// moleAppear4();
-
-// function moleAppear5() {
-//     let active = moleClass[Math.floor(Math.random() * moleClass.length)];
-
-//     active.classList.add("theMole");
-
-//     setTimeout(() => {
-//         active.classList.remove("theMole");
-//         setTimeout(moleAppear5, 500);
-//     }, 1000);
-// }
-// moleAppear5();
-
-// function moleAppear7() {
-//     let active = moleClass[Math.floor(Math.random() * moleClass.length)];
-
-//     active.classList.add("theMole");
-
-//     setTimeout(() => {
-//         active.classList.remove("theMole");
-//         // setTimeout(moleAppear7, 700);
-//     }, 1000);
-// }
-// moleAppear7();
-
-// scoring
-// let score = 0;
-
-// function scoring() {
-
-//     moleAppear.addEventListener("click", moleAppear("splat"));
-//     let score = score + 10;
-
-// }
-// scoring();
-
-// Scoring the game
-
-// timer
-
 // Trevor's Scoreboard
-
 
 let topScore = 0;
 let secondScore = 0;
 let thirdScore = 0;
-let gameBoard = document.querySelectorAll(".theMole");
+let gameBoard = document.querySelectorAll(".Mole");
 gameBoard.forEach((val) => {
     val.addEventListener("click", (e) => {
         hits++;
