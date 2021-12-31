99e4aa28447de89433bbd0f73cf0063614a3bf17
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2021-12-30 19:08:11 -0600

    pop up working, no images via html

--- dist/whack.js
+++ dist/whack.js
@@ -1,71 +1,77 @@
+let topScore = 0;
+let secondScore = 0;
+let thirdScore = 0;
 let hits = 0;
 let score = 0;
 let timer = 59;
 let start = document.getElementById('start');
 let time = document.getElementById('time');
-let wBox2 = document.querySelectorAll(".wBox2")
-let holes = document.querySelectorAll(".hole")
-let moles = document.querySelectorAll(".mole");
+let wBox2 = document.querySelectorAll(".wBox2");
+let holes = document.querySelectorAll(".darkhole");
+let sun = document.querySelectorAll(".sunMole");
+let cash  = document.querySelectorAll(".theCash");
+let chance = [sun, cash];
+let luck =Math.floor(Math.random() * chance.length);
 
 start.addEventListener("click", () => {
-
     document.getElementById('start').style.visibility = "hidden";
     document.getElementsByClassName('wBox2')[0].style.visibility = "visible";
-    let time = window.setInterval(()=> {
-        document.getElementById("time").innerText = ':' +  timer;
+    let time = window.setInterval(() => {
+        document.getElementById("time").innerText = ':' + timer;
         timer--;
     }, 1000);
-    let whereMole = window.setInterval(()=> {
-        // moleLocation();
-        moles = document.querySelectorAll('.sunmole');
-        return moles;
+    let whereMole = window.setInterval(() => {
+        popUps();
+        sun = document.querySelectorAll('.sunMole');
+        return sun;      
     }, 1000);
-    window.setTimeout(()=> {
+    window.setTimeout(() => {
         window.clearInterval(whereMole);
         window.clearInterval(time);
-       // document.getElementById('start').style.visibility = "visible";
-        score =1
+        // document.getElementById('start').style.visibility = "visible";
+        score = 1
         time.innerHTML = 60;
         timer = 59
     }, 60900);
 });
 
-const moleLocation = () => {
-    holes[Math.floor(Math.random() * holes.length)].classList.add('sunmole');
+const popUps = () => {
+     holes[Math.floor(Math.random() * holes.length)].classList.add('sunMole');
+    // chance[luck];
+    // console.log(chance[luck]);
+
 }
 
-holes.forEach((val)=>{
+holes.forEach((val) => {
     val.addEventListener('click', (e) => {
         document.getElementById('score').innerText = score;
-        if (e.target.classList.contains("sunmole")) {
-            e.target.classList.replace("sunmole", "splat")
+        if (e.target.classList.contains("sunMole")) {
+            e.target.classList.replace("sunMole", "splat")
             score++;
-        if (score >= topScore) {
-            topScore = score - 1;
-            document.getElementById("score1").innerText = topScore;
-            console.log(topScore);
-        } else if (score >= secondScore) {
-            secondScore = score - 1;
-            document.getElementById("score2").innerText = score;
-        } else if (score >= thirdScore) {
-            thirdScore = score - 1;
-            document.getElementById("score3").innerText = score;
+            if (score >= topScore) {
+                topScore = score - 1;
+                document.getElementById("score1").innerText = topScore;
+                console.log(topScore);
+            } else if (score >= secondScore) {
+                secondScore = score - 1;
+                document.getElementById("score2").innerText = score;
+            } else if (score >= thirdScore) {
+                thirdScore = score - 1;
+                document.getElementById("score3").innerText = score;
+            }
         }
-    }
-})
+    })
 });
 let resetHoles = window.setInterval(() => {
     let splats = document.querySelectorAll(".splat");
     splats.forEach((val) => {
-        val.classList.replace("splat", "hole");
+        val.classList.replace("splat", "darkhole");
     })
 }, 2000)
 
 // Trevor's Scoreboard
 
-let topScore = 0;
-let secondScore = 0;
-let thirdScore = 0;
+
 let gameBoard = document.querySelectorAll(".Mole");
 gameBoard.forEach((val) => {
     val.addEventListener("click", (e) => {
