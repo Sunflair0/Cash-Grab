50e07e42d2d883aae2d2894f6b02a2adc64be122
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-01-05 15:45:36 -0600

    added round 2 3 4 classes

--- dist/whack.js
+++ dist/whack.js
@@ -7,9 +7,14 @@ let time = document.getElementById('time');
 let wBox2 = document.querySelectorAll(".wBox2");
 let holes = document.querySelectorAll(".darkhole");
 let sun = document.querySelectorAll(".sunMole");
-let cash  = document.querySelectorAll(".theCash");
-let chance = [sun, cash];
-let luck =Math.floor(Math.random() * chance.length);
+let cash1  = document.querySelectorAll(".theCash");
+let daisy  = document.querySelectorAll(".daisyMole");
+let cash2  = document.querySelectorAll(".theLapis");
+let tulip  = document.querySelectorAll(".tulipMole");
+let cash3  = document.querySelectorAll(".theYellow");
+let rose  = document.querySelectorAll(".roseMole");
+let cash4  = document.querySelectorAll(".thePearl");
+
 
 start.addEventListener("click", () => {
     document.getElementById('start').style.visibility = "hidden";
@@ -20,33 +25,42 @@ start.addEventListener("click", () => {
     }, 1000);
     let whereMole = window.setInterval(() => {
         popUps();
-        sun = document.querySelectorAll('.sunMole');
-        return sun;      
-    }, 1000);
+        sun = document.querySelectorAll('.theSun');
+        cash = document.querySelectorAll('.theCash');
+        return sun || cash1;      
+    }, 2000);
     window.setTimeout(() => {
         window.clearInterval(whereMole);
         window.clearInterval(time);
         document.getElementById('start').style.visibility = "visible";
         document.getElementsByClassName('wBox2')[0].style.visibility = "hidden";
         document.getElementById('score').innerText = score;
-        timer = 29
+        timer = 59
     }, 30900);
+    window.setInterval(() => {
+        let clearHole = document.querySelectorAll(".theCash");
+        clearHole.forEach((val) => {
+            val.classList.replace("theCash", "darkhole");
+        })
+    }, 3000)
     window.setInterval(() => {
         let clearHole = document.querySelectorAll(".sunMole");
         clearHole.forEach((val) => {
             val.classList.replace("sunMole", "darkhole");
         })
-    }, 2000)
+    }, 3000)
+
+    
 });
 
 const popUps = () => {
-     holes[Math.floor(Math.random() * holes.length)].classList.add('sunMole');
+     holes[Math.floor(Math.random() * holes.length)].classList.add('theCash');
 }
 holes.forEach((val) => {
     val.addEventListener('click', (e) => {
         document.getElementById('score').innerText = score;
-        if (e.target.classList.contains("sunMole")) {
-            e.target.classList.replace("sunMole", "splat")
+        if (e.target.classList.contains("theCash")) {
+            e.target.classList.replace("theCash", "splat")
             score++;
         }
     })
