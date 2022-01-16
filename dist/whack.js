1c2e6d1df5894e2a783a60327ac1631d1f927898
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-01-16 09:59:50 -0600

    gsap addition, cascade error in gameplay, started roundEnd page

--- dist/whack.js
+++ dist/whack.js
@@ -10,27 +10,31 @@
 
 let score = 0;
 let timer = 29;
-let round =2;
+let round = 1;
 let start = document.getElementById('start');
 let time = document.getElementById('time');
 let wBox2 = document.querySelectorAll(".wBox2");
 let holes = document.querySelectorAll(".darkhole");
 
 let mole1 = document.querySelectorAll(".mole1");
-let mole2  = document.querySelectorAll(".mole2");
-let mole3  = document.querySelectorAll(".mole3");
-let mole4  = document.querySelectorAll(".mole4");
+let mole2 = document.querySelectorAll(".mole2");
+let mole3 = document.querySelectorAll(".mole3");
+let mole4 = document.querySelectorAll(".mole4");
 
-let cash1  = document.querySelectorAll(".cash1");
-let cash2  = document.querySelectorAll(".cash2");
-let cash3  = document.querySelectorAll(".cash3");
-let cash4  = document.querySelectorAll(".cash4");
+let cash1 = document.querySelectorAll(".cash1");
+let cash2 = document.querySelectorAll(".cash2");
+let cash3 = document.querySelectorAll(".cash3");
+let cash4 = document.querySelectorAll(".cash4");
 
-const darkmole = [mole1,mole2,mole3,mole4];
-const darkcash = [cash1,cash2,cash3,cash4];
+const darkmole = [mole1, mole2, mole3, mole4];
+const darkcash = [cash1, cash2, cash3, cash4];
+
+let mole = darkmole[round];
+let cash = darkcash[round];
+let result = 0;
+
 let min = round;
 let max = round + 1;
-
 
 start.addEventListener("click", () => {
     document.getElementById('start').style.visibility = "hidden";
@@ -40,19 +44,14 @@ start.addEventListener("click", () => {
         timer--;
     }, 1000);
     let whereMole = window.setInterval(() => {
-        // popUps();
-
-    
-
-        let result = choice(min, max);
+        result = choice(min, max);
         console.log(result);
-        if (result % 2==0) {
-            proScore()
+        if (result % 2 == 0) {
+            popUpsPlus()
         }
         else {
-            antiScore()
-        };  
-        
+            popUpsMinus()
+        };
     }, 2000);
     window.setTimeout(() => {
         window.clearInterval(whereMole);
@@ -62,124 +61,74 @@ start.addEventListener("click", () => {
         document.getElementById('score').innerText = score;
         timer = 29
     }, 30900);
-    window.setInterval(() => {
-        let clearHole = document.querySelectorAll(`.cash${round}`);
-        console.log(clearHole);
-        clearHole.forEach((val) => {
-            val.classList.replace(`cash${round}`, "darkhole");
-        })
-    }, 3000)
-    window.setInterval(() => {
-        let clearHole = document.querySelectorAll(`.mole${round}`);
-        clearHole.forEach((val) => {
-            val.classList.replace(`mole${round}`, "darkhole");
-        })
-    }, 3000)    
 });
-
-function proScore(){    
-    let whereMole = window.setInterval(() => {        
-           popUpsPlus();
-            cash = document.querySelectorAll(`.cash${round}`);
-           return cash;      
-       }, 2000);
-   window.setTimeout(()=> {
-       window.clearInterval(whereMole);
-       window.clearInterval(time);
-       document.getElementById('start').style.visibility="hidden";
-       document.getElementById('score').innerText=score;
-       timer = 29
-   },30900);
-   window.setInterval(()=> {
-       let clearHole = document.querySelectorAll( `.cash${round}`);
-       clearHole.forEach((val)=> {
-           val.classList.replace( `cash${round}`,"darkhole");
-       })
-   },3000)
-   };
-   
-   holes.forEach((val) => {
-       val.addEventListener('click', (e) => {
-           document.getElementById('score').innerText = score;
-           if (e.target.classList.contains(`cash${round}`)) {
-               e.target.classList.replace( `cash${round}`, "smash")
-               score=score+50;
-           }
-       })
-   });
-   let resetHolesPlus = window.setInterval(() => {
-       let smash = document.querySelectorAll(".smash");
-       smash.forEach((val) => {
-           val.classList.replace("smash", `.cash${round}`);
-       })
-   }, 1000)
-
-function antiScore(){    
-    let whereMole = window.setInterval(() => {        
-           popUpsMinus();
-           mole = document.querySelectorAll(`.mole${round}`);
-           return mole;     
-       }, 2000);
-   window.setTimeout(()=> {
-       window.clearInterval(whereMole);
-       window.clearInterval(time);
-       document.getElementById('start').style.visibility="hidden";
-       document.getElementById('score').innerText=score;
-       timer = 29
-   },30900);
-   window.setInterval(()=> {
-       let clearHole = document.querySelectorAll(`.mole${round}`);
-       clearHole.forEach((val)=> {
-           val.classList.replace(`mole${round}`,"darkhole");
-       })
-   },3000)
-   };
-
-   holes.forEach((val) => {
-    val.addEventListener('click', (e) => {
-        document.getElementById('score').innerText = score;
-        if (e.target.classList.contains(`mole${round}`)) {
-            e.target.classList.replace( `mole${round}`, "splat")
-            score=score-15;
-        }
-    })
-});
-let resetHolesMinus = window.setInterval(() => {
-    let smash = document.querySelectorAll(".splat");
-    smash.forEach((val) => {
-        val.classList.replace("splat", `.mole${round}`);
-    })
-}, 1000)
-
-
-// const popUps = () => {
-//      holes[Math.floor(Math.random() * holes.length)].classList.add(`cash${round}`);
-// }
-// holes.forEach((val) => {
-//     val.addEventListener('click', (e) => {
-//         document.getElementById('score').innerText = score;
-//         if (e.target.classList.contains(`cash${round}`)) {
-//             e.target.classList.replace(`cash${round}`, "splat")
-//             score=+50;
-//         }
-//     })
-// });
-// let resetHoles = window.setInterval(() => {
-//     let splats = document.querySelectorAll(".splat");
-//     splats.forEach((val) => {
-//         val.classList.replace("splat", "darkhole");
-//     })
-// }, 1500)
-
 
 function choice(min, max) {
-    let result =(Math.floor(Math.random() * (max - min + 1))+2);  
+    let result = (Math.floor(Math.random() * (max - min + 1)) + 2);
     return result;
-}
+};
 
 const popUpsMinus = () => {
-    holes[Math.floor(Math.random() * holes.length)].classList.add(`.mole${round}`);
-}
+    holes[Math.floor(Math.random() * holes.length)].classList.add(`mole${round}`);
+    console.log("pop-");
+    {
+        window.setInterval(() => {
+            console.log("clearHole");
+            let clearHole = document.querySelectorAll(`.mole${round}`);
+            clearHole.forEach((val) => {
+                val.classList.replace(`mole${round}`, "darkhole");
+            })
+        }, 3000)
+
+        holes.forEach((val) => {
+            val.addEventListener('click', (e) => {
+                document.getElementById('score').innerText = score;
+                if (e.target.classList.contains(`mole${round}`)) {
+                    e.target.classList.replace(`mole${round}`, "splat")
+                    score = score - 15;
+                }
+            })
+        })
+    }
+};
+
+
 const popUpsPlus = () => {
-    holes[Math.floor(Math.random() * holes.length)].classList.add(`.cash${round}`);
-}
+    holes[Math.floor(Math.random() * holes.length)].classList.add(`cash${round}`);
+    console.log("pop+");
+    {
+        window.setInterval(() => {
+            console.log("clear");
+            let clearHole = document.querySelectorAll(`.cash${round}`);
+            clearHole.forEach((val) => {
+                val.classList.replace(`cash${round}`, "darkhole");
+            })
+        }, 3000)
+        holes.forEach((val) => {
+            val.addEventListener('click', (e) => {
+                document.getElementById('score').innerText = score;
+                if (e.target.classList.contains(`cash${round}`)) {
+                    e.target.classList.replace(`cash${round}`, "smash")
+                    score = score + 50;
+                }
+            })
+        })
+    }
+};
+
+let resetHoles = window.setInterval(() => {
+    let smash = document.querySelectorAll(".smash");
+    smash.forEach((val) => {
+        val.classList.replace("smash", `cash${round}`);
+
+    })
+    let splat = document.querySelectorAll(".splat");
+    splat.forEach((val) => {
+        val.classList.replace("splat", `mole${round}`);
+    })
+}, 1500);
+
+let tl = gsap.timeline();
+
+tl.set(".roundModal",{transformOrigin: "50%, 50%"});
+tl.to(".roundModal",{duration:4, y:500, ease:"bounce", backgroundColor: "#000000bf", border:"5px ridge white", borderRadius: "20%", });
